4d331504080ee7fafffb094542e18941c7dc3349
VictoriaRodriguez22 <victoria.rodriguez@municipiodetandil.edu.ar>
2022-11-14 20:56:48 -0300

    MTermine método determinarEspecie

--- class/classMascotas.ts
+++ class/classMascotas.ts
@@ -12,14 +12,24 @@ class Mascota {
 
     public determinarEspecie () :void {
         
-        if (this.especie == ){
-            console.log ("La Mascota" + this.nombre + "no es un animal exótica")
+        if (this.especie == "gato"){
+            console.log (`La mascota ${this.nombre}  no es un animal exótica`);
+        }
+        
+        else if (this.especie == "perro"){
+            console.log (`La mascota ${this.nombre}  no es un animal exótica`)
         }
 
-    else{
-        console.log ("La Mascota" + this.nombre + "no es un animal exótica")
-    }
-
+        else{
+        console.log (`La mascota ${this.nombre}  es un animal exótica`)
+        }
     }
 
 }
+
+let nuevaMascota : Mascota = new Mascota (373630, "piti" , "gato");
+console.log (nuevaMascota);
+nuevaMascota.determinarEspecie();
+
+let nuevaMascota2 : Mascota = new Mascota (36555, "toro", "tortuga");
+nuevaMascota2.determinarEspecie();
