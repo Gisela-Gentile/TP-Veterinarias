a47f8ca4a0afe321ed9eb41838d5952007a73973
VictoriaRodriguez22 <victoria.rodriguez@municipiodetandil.edu.ar>
2022-11-28 15:50:45 -0300

    Agrego modificaciones

--- class/classMascotas.ts
+++ class/classMascotas.ts
@@ -1,35 +1,32 @@
-
-class Mascota {
-    idClientes : number;
-    nombre : string;
-    especie : string;
-
-    public constructor (idCliente:number, nombre:string, especie:string){
-        this.especie = especie;
-        this.nombre = nombre;
-        this.idClientes = idCliente;
+export default class Mascota {
+    private idMascota : number;
+    private nombre : string;
+    private clase:string;
+    private especie:string;
+     public constructor ( idMascota:number,nombre:string, clase:string, especie:string ){
+      this.idMascota = idMascota
+      this.nombre = nombre;
+      this.clase= clase;
+      this.especie= especie;
+      }
+    public getidMascota():number{
+      return this.idMascota
     }
+     public getnombre():string{
+      return this.nombre;
+     }
+     public getclase():string{
+      return this.clase
+     }
+      public getespecie():string{
+       return this.especie;}
+      }
 
-    public determinarEspecie () :void {
-        
-        if (this.especie == "gato"){
-            console.log (`La mascota ${this.nombre}  no es un animal exótica`);
-        }
-        
-        else if (this.especie == "perro"){
-            console.log (`La mascota ${this.nombre}  no es un animal exótica`)
-        }
-
-        else{
-        console.log (`La mascota ${this.nombre}  es un animal exótica`)
-        }
-    }
-
-}
-
-let nuevaMascota : Mascota = new Mascota (373630, "piti" , "gato");
-console.log (nuevaMascota);
-nuevaMascota.determinarEspecie();
-
-let nuevaMascota2 : Mascota = new Mascota (36555, "toro", "tortuga");
-nuevaMascota2.determinarEspecie();
+  
+  
+  
+  
+  
+  
+  
+  
