0700e7b5f4b5b58a2c0ea7e6ac2e1e51b6485394
VictoriaRodriguez22 <victoria.rodriguez@municipiodetandil.edu.ar>
2022-11-29 14:11:59 -0300

    Agrego clase veterinaria

--- class/veterinaria.ts
+++ class/veterinaria.ts
@@ -70,4 +70,4 @@ export default class Veterinaria implements DatoRed{
         public mostrarUnCliente( posicion:number):void{
             console.log(this.listaClientes[posicion]);
                 }
-            }s
+            }
